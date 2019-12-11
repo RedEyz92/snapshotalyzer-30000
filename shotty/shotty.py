e117a9317b0eb7d9e5d2fdc7d06314318458d0af
Ricardo Abreu <ricardo@Mac.local>
2019-12-11 12:01:22 -0500

    Added reboot command for instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -38,7 +38,7 @@ def list_snapshots(project, list_all):
 
     instances = filter_instances(project)
 
-    for i in list_instances:
+    for i in instances:
         for v in i.volumes.all():
             for s in v.snapshots.all():
                 print(",".join((
@@ -166,6 +166,24 @@ def start_instances(project):
             i.start()
         except botocore.exceptions.ClientError as e:
             print (" Could not start {0}. ".format(i.id) + str(e))
+            continue
+
+    return
+
+@instances.command('reboot')
+@click.option('--project', default=None,
+    help='Onlly instances for project')
+def reboot_instances(project):
+    "Reboot EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Rebooting {0}...".format(i.id))
+        try:
+            i.reboot()
+        except botocore.exceptions.ClientError as e:
+            print (" Could not reboot {0}. ".format(i.id) + str(e))
             continue
 
     return
