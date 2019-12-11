720214c70875f90fb54ff8c7a6c35d81a9c38ac1
Ricardo Abreu <ricardo@Mac.local>
2019-12-10 20:19:03 -0500

    Fixed typo in shotty.py and fixed pipenv on mac

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -98,7 +98,7 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
-            if has_pending snapshot(v):
+            if has_pending_snapshot(v):
                 print("  Skipping {0}, snapshot already in progress".format(v.id))
                 continue
 
